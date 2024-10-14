ba7f2e628fe7a7984b2f0a89157c9032b8a0f857
esther guerra <esther.guerra@uam.es>
2024-10-14 17:02:23 +0200

    added 9 mutation operators

--- src/mutator.py
+++ src/mutator.py
@@ -1,13 +1,15 @@
 import os.path
 from spec import (ResponseElement, ExecuteElement, Action, ActionModule, DataGatheringModule, QuestionAnswer,
-                  QuestionAnsweringModule, SequenceItem, ToolItem, AnswerItem, MenuModule, Module)
+                  QuestionAnsweringModule, SequenceItem, ToolItem, AnswerItem, MenuModule, Module, MemoryScope,
+                  SequenceModule)
 from spec import parse_yaml
 import yaml
 import glob
 import shutil
+import re
 from argparse import ArgumentParser
 from typing import Dict
-
+from abc import ABC, abstractmethod
 
 # --------------------------------------------------------------------------------------------
 # TODO: mutatest for python (https://mutatest.readthedocs.io/en/latest/commandline.html)
@@ -15,10 +17,11 @@ from typing import Dict
 # --------------------------------------------------------------------------------------------
 # "to_dict" methods for the chatbot components, to facilitate their persistence using pydantic
 
+
 def to_dict_response_element(self) -> dict:
     ret = dict()
     ret['text'] = self.text
-    if self.rephrase is not None:
+    if self.rephrase:
         ret['rephrase'] = self.rephrase
     return ret
 
@@ -32,10 +35,13 @@ def to_dict_execute_element(self) -> dict:
 
 def to_dict_action(self) -> dict:
     ret = dict()
-    if self.execute is not None:
+    if self.execute:
         ret['execute'] = self.execute.to_dict()
-    if self.response is not None:
-        ret['response'] = self.response.to_dict()
+    if self.response:
+        if isinstance(self.response, ResponseElement):
+            ret['response'] = self.response.to_dict()
+        else:
+            ret['response'] = self.response
     return ret
 
 
@@ -71,13 +77,16 @@ def to_dict_question_answering_module(self) -> dict:
     ret['kind'] = self.kind
     ret['description'] = self.description
     ret['questions'] = [question.to_dict() for question in self.questions]
+    if self.on_success:
+        ret['on-success'] = self.on_success.to_dict()
     return ret
 
 
 def to_dict_sequence_item(self) -> dict:
     ret = dict()
     ret['title'] = self.title
-    ret['memory'] = self.memory.value
+    if self.memory:
+        ret['memory'] = self.memory.value
     ret['kind'] = self.kind
     ret['references'] = self.references
     return ret
@@ -104,7 +113,7 @@ def to_dict_menu_module(self) -> dict:
     ret['name'] = self.name
     ret['kind'] = self.kind
     ret['presentation'] = self.presentation
-    if self.fallback is not None:
+    if self.fallback:
         ret['fallback'] = self.fallback
     ret['items'] = [item.to_dict() for item in self.items]
     return {'modules': [ret]}
@@ -126,14 +135,18 @@ setattr(MenuModule, "to_dict", to_dict_menu_module)
 # --------------------------------------------------------------------------------------------
 # mutation operators
 
-class BaseMutationOperator:
+class BaseMutationOperator(ABC):
     def __init__(self, chatbot_folder: str, mutants_folder: str):
         self.counter = 1
         self.name = self.__class__.__name__
         self.chatbot_folder = chatbot_folder
         self.mutants_folder = mutants_folder
 
-    def persist(self, modules: Dict[str, Module], description: str = None):
+    @abstractmethod
+    def generate_mutants(self, modules: Dict[str, Module]) -> None:
+        pass
+
+    def persist(self, modules: Dict[str, Module], description: str = None) -> None:
         # copy original chatbot in output folder
         output_folder = f"{self.mutants_folder}/{self.name}_{self.counter}"
         shutil.copytree(self.chatbot_folder, output_folder)
@@ -149,26 +162,13 @@ class BaseMutationOperator:
                 file.write(description)
 
 
-class DeleteFallback(BaseMutationOperator):
-    def generate_mutants(self, modules: Dict[str, Module]) -> None:
-        for module_file, module in modules.items():
-            if isinstance(module, MenuModule) and module.fallback is not None:
-                fallback = module.fallback
-                module.fallback = None
-                self.persist(
-                    {module_file: module},  # for efficiency, we only persist the mutated module
-                    f"Mutated file: {module_file}\nMutation description: The fallback has been deleted"
-                )
-                module.fallback = fallback
-
-
 class DeleteEnumDataValue(BaseMutationOperator):
     def generate_mutants(self, modules: Dict[str, Module]) -> None:
         for module_file, module in modules.items():
             if isinstance(module, (ActionModule, DataGatheringModule)):
                 for data in module.data:
                     for data_name, data_value in data.items():
-                        if data_value.get('type') == 'enum':
+                        if isinstance(data_value, dict) and data_value.get('type') == 'enum':
                             values = data_value.get('values')
                             for i in range(len(values)):
                                 data_value['values'] = values[:i] + values[i+1:]
@@ -179,10 +179,214 @@ class DeleteEnumDataValue(BaseMutationOperator):
                             data_value['values'] = values
 
 
+class ChangeRequiredData(BaseMutationOperator):
+    def generate_mutants(self, modules: Dict[str, Module]) -> None:
+        for module_file, module in modules.items():
+            if isinstance(module, (ActionModule, DataGatheringModule)):
+                for data in module.data:
+                    for data_name, data_value in data.items():
+                        if isinstance(data_value, dict) and 'required' in data_value:
+                            data_value['required'] = not data_value['required']
+                            self.persist(
+                                {module_file: module},  # for efficiency, we only persist the mutated module
+                                f"Mutated file: {module_file}\nMutation description: The data '{data_name}' has changed from required={not data_value['required']} to required={data_value['required']}"
+                            )
+                            data_value['required'] = not data_value['required']
+
+
+class DeleteQuestionAnswer(BaseMutationOperator):
+    def generate_mutants(self, modules: Dict[str, Module]) -> None:
+        for module_file, module in modules.items():
+            if isinstance(module, QuestionAnsweringModule):
+                questions = module.questions
+                for i in range(len(module.questions)):
+                    module.questions = questions[:i] + questions[i+1:]
+                    self.persist(
+                        {module_file: module},  # for efficiency, we only persist the mutated module
+                        f"Mutated file: {module_file}\nMutation description: The following question/answer has been deleted\n- question: {questions[i].question}\n  answer: {questions[i].answer}"
+                    )
+                module.questions = questions
+
+
+class SwapQuestionAnswer(BaseMutationOperator):
+    """ It swaps the answer of two questions in a question-answering module """
+    def generate_mutants(self, modules: Dict[str, Module]) -> None:
+        for module_file, module in modules.items():
+            if isinstance(module, QuestionAnsweringModule):
+                questions = module.questions
+                for i in range(len(module.questions)-1):
+                    for j in range(i+1, len(module.questions)):
+                        answer_i = module.questions[i].answer
+                        answer_j = module.questions[j].answer
+                        module.questions[i].answer = answer_j
+                        module.questions[j].answer = answer_i
+                        self.persist(
+                            {module_file: module},  # for efficiency, we only persist the mutated module
+                            f"Mutated file: {module_file}\nMutation description: The answer of the following questions has been swapped\n- question: {questions[i].question}\n  answer: {answer_i}\n- question: {questions[j].question}\n  answer: {answer_j}"
+                        )
+                        module.questions[i].answer = answer_i
+                        module.questions[j].answer = answer_j
+                module.questions = questions
+
+
+class DeleteFallback(BaseMutationOperator):
+    def generate_mutants(self, modules: Dict[str, Module]) -> None:
+        for module_file, module in modules.items():
+            if isinstance(module, MenuModule) and module.fallback:
+                fallback = module.fallback
+                module.fallback = None
+                self.persist(
+                    {module_file: module},  # for efficiency, we only persist the mutated module
+                    f"Mutated file: {module_file}\nMutation description: The fallback has been deleted"
+                )
+                module.fallback = fallback
+
+
+class DeleteItemTopModule(BaseMutationOperator):
+    def generate_mutants(self, modules: Dict[str, Module]) -> None:
+        for module_file, module in modules.items():
+            if isinstance(module, MenuModule):
+                items = module.items
+                for i in range(len(module.items)):
+                    module.items = items[:i] + items[i+1:]
+                    self.persist(
+                        {module_file: module},  # for efficiency, we only persist the mutated module
+                        f"Mutated file: {module_file}\nMutation description: The item with the following title has been deleted: '{items[i].title}'"
+                    )
+                module.items = items
+
+
+class DeleteSequenceStep(BaseMutationOperator):
+    def generate_mutants(self, modules: Dict[str, Module]) -> None:
+        for module_file, module in modules.items():
+            if isinstance(module, MenuModule):
+                for item in module.items:
+                    if isinstance(item, SequenceItem):
+                        references = item.references
+                        for i in range(len(item.references)):
+                            item.references = references[:i] + references[i+1:]
+                            self.persist(
+                                {module_file: module},  # for efficiency, we only persist the mutated module
+                                f"Mutated file: {module_file}\nMutation description: The step {references[i]} has been deleted"
+                            )
+                        item.references = references
+
+
+class SwapSequenceStep(BaseMutationOperator):
+    """ It swaps the order of two steps in a sequence """
+    def generate_mutants(self, modules: Dict[str, Module]) -> None:
+        for module_file, module in modules.items():
+            if isinstance(module, MenuModule):
+                for item in module.items:
+                    if isinstance(item, SequenceItem):
+                        references = item.references
+                        for i in range(len(item.references)-1):
+                            for j in range(i+1, len(item.references)):
+                                reference_i = item.references[i]
+                                reference_j = item.references[j]
+                                item.references[i] = reference_j
+                                item.references[j] = reference_i
+                                self.persist(
+                                    {module_file: module},  # for efficiency, we only persist the mutated module
+                                    f"Mutated file: {module_file}\nMutation description: The steps {reference_i} and {reference_j} have been swapped"
+                                )
+                                item.references[i] = reference_i
+                                item.references[j] = reference_j
+                        item.references = references
+
+
+class DeleteDataFromResponse(BaseMutationOperator):
+    """ Each mutant removes an expression between curly braces from a response text """
+    def generate_mutants(self, modules: Dict[str, Module]) -> None:
+        for module_file, module in modules.items():
+            if (isinstance(module, (DataGatheringModule, ActionModule, QuestionAnsweringModule)) and
+                    module.on_success and
+                    isinstance(module.on_success, Action)):
+                text = self.get_text(module.on_success)
+                fragments = re.split(r'(\{{1,2}.*?\}{1,2})', text) # deals with 1 or 2 curly braces
+                for i in range(len(fragments)):
+                    if fragments[i].startswith('{') and fragments[i].endswith('}'):
+                        self.set_text(module.on_success, ''.join(fragments[:i] + fragments[i+1:]))
+                        self.persist(
+                            {module_file: module},  # for efficiency, we only persist the mutated module
+                            f"Mutated file: {module_file}\nMutation description: The expression {fragments[i]} has been removed from the text response"
+                        )
+                self.set_text(module.on_success, text)
+
+    def get_text(self, action: Action) -> str:
+        if isinstance(action.response, ResponseElement):
+            return action.response.text
+        elif isinstance(action.response, str):
+            return action.response
+        return ''
+
+    def set_text(self, action: Action, new_text: str) -> None:
+        if isinstance(action.response, ResponseElement):
+            action.response.text = new_text
+        elif isinstance(action.response, str):
+            action.response = new_text
+
+
+class ChangeRephrase(BaseMutationOperator):
+    def generate_mutants(self, modules: Dict[str, Module]) -> None:
+        for module_file, module in modules.items():
+            if (isinstance(module, (DataGatheringModule, ActionModule, QuestionAnsweringModule)) and
+                    module.on_success and
+                    isinstance(module.on_success, Action) and
+                    isinstance(module.on_success.response, ResponseElement)):
+                rephrase = module.on_success.response.rephrase
+                if rephrase != "direct":
+                    module.on_success.response.rephrase = "direct"
+                    self.persist(
+                        {module_file: module},  # for efficiency, we only persist the mutated module
+                        f"Mutated file: {module_file}\nMutation description: The rephrase has changed from {rephrase} to direct"
+                    )
+                if rephrase != "simple":
+                    module.on_success.response.rephrase = "simple"
+                    self.persist(
+                        {module_file: module},  # for efficiency, we only persist the mutated module
+                        f"Mutated file: {module_file}\nMutation description: The rephrase has changed from {rephrase} to simple"
+                    )
+                if rephrase != "on-caller" and rephrase != "on_caller":
+                    module.on_success.response.rephrase = "on-caller"
+                    self.persist(
+                        {module_file: module},  # for efficiency, we only persist the mutated module
+                        f"Mutated file: {module_file}\nMutation description: The rephrase has changed from {rephrase} to on-caller"
+                    )
+                module.on_success.response.rephrase = rephrase
+
+
+class ChangeMemoryScope(BaseMutationOperator):
+    def mutate(self, element_with_memory: SequenceItem | SequenceModule, module_file: str, module: Module) -> None:
+        if element_with_memory.memory:
+            memory = element_with_memory.memory
+            if element_with_memory.memory == MemoryScope.individual:
+                element_with_memory.memory = MemoryScope.full
+            else:
+                element_with_memory.memory = MemoryScope.individual
+            self.persist(
+                {module_file: module},  # for efficiency, we only persist the mutated module
+                f"Mutated file: {module_file}\nMutation description: The memory has changed from {memory} to {element_with_memory.memory} for the sequence with title '{element_with_memory.title}'"
+            )
+            element_with_memory.memory = memory
+
+    def generate_mutants(self, modules: Dict[str, Module]) -> None:
+        for module_file, module in modules.items():
+            if isinstance(module, MenuModule):
+                for item in module.items:
+                    if isinstance(item, SequenceItem):
+                        self.mutate(item, module_file, module)
+            elif isinstance(module, SequenceModule):
+                self.mutate(module, module_file, module)
+
 # --------------------------------------------------------------------------------------------
 
 
 def generate_mutants(chatbot_folder: str, mutants_folder: str):
+    if not os.path.exists(chatbot_folder):
+        print(f"The folder {chatbot_folder} does not exist")
+        exit(1)
+
     if os.path.exists(mutants_folder):
         shutil.rmtree(mutants_folder)
 
@@ -196,8 +400,17 @@ def generate_mutants(chatbot_folder: str, mutants_folder: str):
 
     # generate mutants
     mutation_operators = [
-        DeleteFallback(chatbot_folder, mutants_folder),
         DeleteEnumDataValue(chatbot_folder, mutants_folder),
+        ChangeRequiredData(chatbot_folder, mutants_folder),
+        DeleteQuestionAnswer(chatbot_folder, mutants_folder),
+        SwapQuestionAnswer(chatbot_folder, mutants_folder),
+        DeleteFallback(chatbot_folder, mutants_folder),
+        DeleteItemTopModule(chatbot_folder, mutants_folder),
+        DeleteSequenceStep(chatbot_folder, mutants_folder),
+        SwapSequenceStep(chatbot_folder, mutants_folder),
+        DeleteDataFromResponse(chatbot_folder, mutants_folder),
+        ChangeRephrase(chatbot_folder, mutants_folder),
+        ChangeMemoryScope(chatbot_folder, mutants_folder),
     ]
     for operator in mutation_operators:
         operator.generate_mutants(modules)
